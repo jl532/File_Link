a6d504da4cf318f7b20f9ec52ec1a26c022decc0
Jason Liu <jl532@duke.edu>
2017-12-12 21:45:45 -0500

    make the class that will contain the information.. I'll need to study up on how to build a tree/linked list for this

--- FileLink_ProofOfConcept.py
+++ FileLink_ProofOfConcept.py
@@ -1,7 +1,25 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Dec 12 20:50:08 2017
+import tkinter as tk
+from tkinter import filedialog
 
-@author: Mars
-"""
+class FileInstance:
+    "a class that carries the file name, directory, parent/child info"
+    def __init__(self, fileName, filePath,
+                 ID, parents, children):
+        self.fileName = fileName
+        self.filePath = filePath
+        self.ID = ID
+        self.parents = parents
+        self.children = children
+    def displayAllInfo(self):
+        # need to add in checks for missing children/parents
+        # currently just using -99 for missing values
+        print("File Name: " + str(fileName) +
+              " | File Path: " + str(filePath) +
+              " | Parents: " + str(parents) +
+              " | Children: " + str(children))
 
+root = tk.Tk()
+root.withdraw()
+
+file_path = filedialog.askopenfilename()
+print(file_path)
